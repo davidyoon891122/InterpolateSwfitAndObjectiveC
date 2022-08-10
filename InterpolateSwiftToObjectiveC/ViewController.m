816f16faef5fd38b3ea3229a4808dba418d24d45
Yoonjiwon <k112751@koreainvestment.com>
2022-08-10 09:38:04 +0900

    Add a default button for testing

--- InterpolateSwiftToObjectiveC/ViewController.m
+++ InterpolateSwiftToObjectiveC/ViewController.m
@@ -13,10 +13,33 @@
 
 @implementation ViewController
 
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    self.view.backgroundColor = [UIColor redColor];
+    self.view.backgroundColor = [UIColor systemBackgroundColor];
+    [self createButton];
+}
+
+- (void)createButton {
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
+    button.translatesAutoresizingMaskIntoConstraints = NO;
+    [button setTitle:@"DefaultButton" forState:UIControlStateNormal];
+    [button
+     addTarget:self
+     action: @selector(didTapButtonAction)
+     forControlEvents:UIControlEventTouchUpInside];
+
+    [self.view addSubview:button];
+
+    [button.topAnchor constraintEqualToAnchor: self.view.safeAreaLayoutGuide.topAnchor constant: 0.0].active = YES;
+    [button.leadingAnchor constraintEqualToAnchor: self.view.leadingAnchor constant:0.0].active = YES;
+//    [button.widthAnchor constraintEqualToConstant:].active = YES;
+}
+
+- (void)didTapButtonAction {
+    NSLog(@"didTapButtonAction");
 }
 
 
