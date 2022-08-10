c82a1a2d60f7dff7cba7336ad2d68017a713a29a
Yoonjiwon <k112751@koreainvestment.com>
2022-08-10 09:10:42 +0900

    Set a default skeleton
    * Set default settings

--- InterpolateSwiftToObjectiveC/SceneDelegate.h
+++ InterpolateSwiftToObjectiveC/SceneDelegate.h
@@ -6,10 +6,10 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "ViewController.h"
 
 @interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
 
 @property (strong, nonatomic) UIWindow * window;
-
 @end
 
